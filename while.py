96dd4327b9514115b32796d481853277701660d0
Cesar_Rosas <cezar.darkangel@hotmail.com>
2020-10-27 12:56:35 -0600

    while script

---
+++ while.py
@@ -0,0 +1,10 @@
+"""Clase de while"""
+
+count = 0
+loop = True
+
+while count < 100:
+    print(count)
+    count += 1
+else:
+    print(count) #Si no se cumple el while, se pasa directamente al else
